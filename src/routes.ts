4d1f50dc1bfc956af5eeb6b4800f10693b90df44
pablo <68402065+itzinn@users.noreply.github.com>
2023-10-17 16:09:08 -0300

    mudanças nos arquivos comando npx prettier --write

--- src/routes.ts
+++ src/routes.ts
@@ -5,9 +5,8 @@ const router = Router();
 
 //router.get('/', new HomeController().hello);
 
-router.get('/', function(req, res){
-    res.send('hi');
+router.get('/', function (req, res) {
+  res.send('hi');
 });
-
 
 export default router;
