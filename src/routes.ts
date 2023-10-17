4f0332e70123dca6891ed45b08cde8bfa2a59a55
pablo <68402065+itzinn@users.noreply.github.com>
2023-10-17 15:59:04 -0300

    ajuste de configurações para uso de MySQL

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,11 @@ import { HomeController } from './controller/HomeController';
 
 const router = Router();
 
-router.get('/', new HomeController().hello);
+//router.get('/', new HomeController().hello);
+
+router.get('/', function(req, res){
+    res.send('hi');
+});
+
 
 export default router;
