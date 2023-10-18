15f0dac11ae549a3dfa43a7b715f046beae638bc
skrowll <lucasdejesus.rocha@hotmail.com>
2023-10-18 14:13:11 -0300

    feat: add list of migrations and set migrationRun for true

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,22 +1,13 @@
 import { DataSource } from 'typeorm';
 
-export const MongoDataSource = new DataSource({
-  name: 'default',
-  type: 'mongodb',
-  authSource: 'admin',
-  database: process.env.DB_DATABASE,
-  url: process.env.DB_CONNECTION_STRING,
-  entities: ['src/entity/*.ts', 'entity/*.js'],
-  logging: true,
-  synchronize: true
-});
-
 export const MysqlDataSource = new DataSource({
   name: 'default',
   type: 'mysql',
   database: process.env.DB_DATABASE,
   url: process.env.DB_CONNECTION_STRING,
   entities: ['src/entity/*.ts', 'entity/*.js'],
+  migrations: ['src/migration/*.ts', 'migration/*.js'],
   logging: true,
-  synchronize: true
+  synchronize: true,
+  migrationsRun: true
 });
