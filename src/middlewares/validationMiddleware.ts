b2da4013d9ab7e9be7cbc2d4e3d0af5d23e4c894
Ricardo Miranda de Novais <ricardonovais1@gmail.com>
2023-10-19 17:52:16 -0300

    Add TSDocs às funções do validationMiddleware

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -1,11 +1,28 @@
+/**
+ *Calls functions to validate email and password
+ * @param email inserted by user when trying to authenticate.
+ * @param password inserted by user when trying to authenticate.
+ * @returns {boolean} If password or email format is invalid it returns false
+ */
 export function validateEmailAndPassword(email, password): boolean {
   return !validateEmail(email) || !validatePassword(password);
 }
 
+/**
+ * Validates email format
+ * @param email inserted by user when trying to authenticate.
+ * @returns {boolean} If password format is valid or not
+ */
 function validateEmail(email): boolean {
   return /\S+@\S+\.\S+/.test(email);
 }
 
+/**
+ * Validates password format
+ * @param password inserted by user when trying to authenticate.
+ * @returns {boolean} If password format is valid or not
+ * (Minimum 8 chars, 1 uppercase, 1 lowercase, 1 number and 1 special character)
+ */
 function validatePassword(password): boolean {
   return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/.test(password);
 }
