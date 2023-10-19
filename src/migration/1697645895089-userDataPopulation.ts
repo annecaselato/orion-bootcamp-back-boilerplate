22726d8cfe7e488d0d1e9b75b9b0b2383bc369b6
skrowll <lucasdejesus.rocha@hotmail.com>
2023-10-19 11:56:11 -0300

    feat: change users password and add TSDoc

--- src/migration/1697645895089-userDataPopulation.ts
+++ src/migration/1697645895089-userDataPopulation.ts
@@ -3,29 +3,39 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 const USERS = [
   {
     email: 'user1@test.com',
-    password: '123456'
+    password: 'Pa$$word123'
   },
   {
     email: 'user2@test.com',
-    password: 'abcdef'
+    password: 'Pa$$word321'
   }
 ];
 
 export class UserDataPopulation1697645895089 implements MigrationInterface {
+  /**
+   * Executa as operações de inserção de dados na tabela "Users" durante a migração.
+   *
+   * @param queryRunner - O QueryRunner utilizado para executar as consultas no banco de dados.
+   * @returns Uma Promise que é resolvida quando a operação é concluída.
+   */
   public async up(queryRunner: QueryRunner): Promise<void> {
     for (const user of USERS) {
-      await queryRunner.query(
-        'INSERT INTO users (email, password) VALUES (?, ?)',
-        [user.email, user.password]
-      );
+      await queryRunner.manager.save('Users', {
+        email: user.email,
+        password: user.password
+      });
     }
   }
 
+  /**
+   * Executa as operações de exclusão de dados na tabela "Users" durante a reversão da migração.
+   *
+   * @param queryRunner - O QueryRunner utilizado para executar as consultas no banco de dados.
+   * @returns Uma Promise que é resolvida quando a operação é concluída.
+   */
   public async down(queryRunner: QueryRunner): Promise<void> {
     for (const user of USERS) {
-      await queryRunner.query('DELETE FROM users WHERE email = ?', [
-        user.email
-      ]);
+      await queryRunner.manager.delete('Users', { email: user.email });
     }
   }
 }
