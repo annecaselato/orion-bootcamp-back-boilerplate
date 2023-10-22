206477a61cb46c76dd72a8db237bc28ce5d70f30
pablo <68402065+itzinn@users.noreply.github.com>
2023-10-22 20:58:24 -0300

    feat(api): criação do endpoint de login

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import { HomeController } from './controller/HomeController';
+import { AuthController } from './controller/AuthController';
 
 const router = Router();
 
-//router.get('/', new HomeController().hello);
-
 router.get('/', function (req, res) {
-  res.send('hi');
+  res.send('esta é a pagina principal');
 });
+
+router.get('/login', function (req, res) {
+  res.send('esta é a pagina de login');
+});
+
+router.post('/login', new AuthController().login);
 
 export default router;
