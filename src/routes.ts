d9efada7611deb5ae62ab293e2c16244fcb10f3d
joaogabriellyra <jgabriellyra@hotmail.com>
2023-10-22 20:40:01 -0300

    adicionada a rota post para recuperar senha do usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import { HomeController } from './controller/HomeController';
+import { UserController } from './controller/UserController';
+import { UserService } from './service/UserService';
+import { MysqlDataSource } from './config/database';
 
 const router = Router();
+const userController = new UserController(new UserService(MysqlDataSource));
 
 router.get('/', new HomeController().hello);
+router.post('/', (req, res) => userController.recoverPassword(req, res));
 
 export default router;
