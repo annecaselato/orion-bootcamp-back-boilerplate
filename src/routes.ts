7089d9875f161ee0c2543b312fc116a82e3c60fe
Nickolas Luan <Nickolasluan15@gmail.com>
2023-10-24 01:21:01 -0300

    Update modules

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,5 @@
 import { Router, Request, Response } from 'express';
 import { HomeController } from './controller/HomeController';
-import { EmailSender } from './library/mail';
 
 const router = Router();
 
