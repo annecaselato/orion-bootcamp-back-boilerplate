a58be3c35740de7f2d56928971a1e61a81bafd58
pablo <68402065+itzinn@users.noreply.github.com>
2023-10-25 22:04:25 -0300

    feat: redefine colunas da entidade usuário

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -1,12 +1,47 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  Entity,
+  Column,
+  BeforeInsert,
+  BeforeUpdate,
+  PrimaryGeneratedColumn
+} from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 250, unique: true })
   email: string;
 
-  @Column()
+  @Column({ type: 'varchar', length: 72 })
   password: string;
+
+  @Column({ type: 'varchar', length: 150 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 30 }) // deveria ser number ?
+  gender: string;
+
+  @Column()
+  birth_date: Date;
+
+  @Column({ update: false })
+  created_at: Date;
+
+  @BeforeInsert()
+  createdAtDate() {
+    this.created_at = new Date();
+    this.last_update = new Date();
+  }
+
+  @Column()
+  last_update: Date;
+
+  @BeforeUpdate()
+  updateDates() {
+    this.last_update = new Date();
+  }
 
   @Column({ default: false })
   isActivated: boolean;
