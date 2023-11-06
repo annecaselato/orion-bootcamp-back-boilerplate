59f19c190bfed73eaddf0f44fa67b6ddd7804afc
skrowll <lucasdejesus.rocha@hotmail.com>
2023-11-06 19:59:00 -0300

    feat: adds function verifyJWTToken on jwtUtils

--- src/library/jwtUtils.ts
+++ src/library/jwtUtils.ts
@@ -12,4 +12,19 @@ export class JwtUtils {
   public static generateJWTToken(data: object, expiresIn: string): Promise<string> {
     return jwt.sign(data, secretKey, { expiresIn });
   }
+
+  /**
+   * Verifies a JWT token for authentication.
+   * @param token - The JWT token to be verified.
+   * @returns An object representing the verified data or a JWT verification error.
+   * @throws If the token is invalid or an error occurs during verification, an error object is returned.
+   */
+  public static verifyJWTToken(token: string): object | jwt.VerifyErrors {
+    try {
+      const verified = jwt.verify(token, secretKey);
+      return verified;
+    } catch (error) {
+      return { error };
+    }
+  }
 }
