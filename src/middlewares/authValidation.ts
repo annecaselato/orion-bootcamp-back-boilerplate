fedf8d554f76598540bb6d3b093af58b0b8fc30e
skrowll <lucasdejesus.rocha@hotmail.com>
2023-11-06 20:00:17 -0300

    fix: remove findUserByEmail and add findUserById

--- src/middlewares/authValidation.ts
+++ src/middlewares/authValidation.ts
@@ -7,31 +7,34 @@ import { UserRepository } from '../repositories/userRepository';
  * @param req - The Express request object.
  * @param res - The Express response object.
  * @param next - The next middleware function.
- * @returns A Promise that resolves if the authentication is successful or sends an error response if it fails.
  */
 export async function authValidation(req: Request, res: Response, next: NextFunction): Promise<void> {
   const accessToken = req.headers.authorization;
-  const { email } = req.body;
+  const verifiedToken = await JwtUtils.verifyJWTToken(accessToken);
+
+  if (verifiedToken.error) {
+    const errorMessage =
+      {
+        'jwt must be provided': 'Access token deve ser fornecido',
+        'invalid signature': 'Access token inválido',
+        'jwt expired': 'Access token expirado'
+      }[verifiedToken.error.message] || 'Não autorizado';
+
+    res.status(401).json({ message: errorMessage });
+    return;
+  }
+
+  const userId = verifiedToken.id;
 
   try {
-    const user = await UserRepository.findUserByEmail(email);
+    const user = await UserRepository.findUserById(userId);
 
-    const verifiedToken = await JwtUtils.verifyJWTToken(accessToken);
-
-    if (verifiedToken.error) {
-      const errorMessage =
-        {
-          'jwt must be provided': 'Access token deve ser fornecido',
-          'invalid signature': 'Access token inválido',
-          'jwt expired': 'Access token expirado'
-        }[verifiedToken.error.message] || 'Não autorizado';
-
-      res.status(401).json({ message: errorMessage });
-    } else if (user.accessToken !== accessToken) {
+    if (user.accessToken !== accessToken) {
       res.status(401).json({ message: 'Access token inválido' });
-    } else {
-      next();
+      return;
     }
+
+    next();
   } catch (error) {
     res.status(500).json({ message: 'Erro interno do servidor' });
   }
