7ca8a8b93f83b59636ab719a2ab47dba3d9edd27
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2023-11-07 20:16:15 -0300

    feat(job_Update_Database): iniciando prototipo do job

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import swaggerUI from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
 import { MysqlDataSource } from './config/database';
 import { swaggerConfig } from './config/swagger';
+import CharactersHandler from './handlers/CharactersHandler';
+import FormatHandler from './handlers/FormatHandler';
+const cron = require("node-cron");
 import routes from './routes';
 
 MysqlDataSource.initialize()
@@ -19,6 +22,21 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 app.use(routes);
+
+cron.schedule('* 1 * * *', function charactersUpdateSchedule () {
+  console.log('Running task to update database every 1 hour');
+  try{
+    const charactersData = new CharactersHandler().getCharacters();
+    try{
+      const charactersTranslated = new FormatHandler().extractCharactersAndTryTotranslate(charactersData);
+    }catch(error){
+
+    }
+
+  }catch(error){
+    console.error('Unable to update database. Trying again in 1 hour');
+  }
+});
 
 const swaggerSpec = swaggerJSDoc(swaggerConfig);
 
