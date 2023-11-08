54cdf6c09fd68bcc38fc436ec16f84139b945652
Ricardo Miranda de Novais <ricardonovais1@gmail.com>
2023-11-08 18:44:32 -0300

    refactor: generateJWTToken

--- src/library/jwtUtils.ts
+++ src/library/jwtUtils.ts
@@ -4,7 +4,10 @@ const secretKey = process.env.JWT_SECRET;
 
 export class JwtUtils {
   /**
-   *Generates jwt token, using 'jwt.sign' function. Static to allow to be called inside class itself.
+   * generateJWTToken
+   *
+   * Generates jwt token, using 'jwt.sign' function. Static to allow to be called inside class itself.
+   *
    * @param data instance of user to which the token will be assigned.
    * @param expiresIn Time taken until token expires.
    * @returns {Promise<string>} JWT token (string), returned from asynchronously from jwt api.
