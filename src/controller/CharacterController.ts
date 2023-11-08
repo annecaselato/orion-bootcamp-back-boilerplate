a759a4b8c145e0d251acaf6c9886d79464e8b7e5
pablo <68402065+itzinn@users.noreply.github.com>
2023-11-08 08:36:52 -0300

    docs: adiciona documentação swagger para endpoint /v1/getCharacters/:page

--- src/controller/CharacterController.ts
+++ src/controller/CharacterController.ts
@@ -2,6 +2,81 @@ import { Request, Response } from 'express';
 import { MysqlDataSource } from '../config/database';
 import { Character } from '../entity/Character';
 
+/**
+ * @swagger
+ *
+ * /v1/getCharacters/{page}:
+ *   get:
+ *     summary: Requisita páginas de personagens
+ *     description: Retorna uma quantidade variável de personagens a depender da página selecionada
+ *     security:
+ *       - BearerAuth: []
+ *     tags: [Characters]
+ *     parameters:
+ *       - in: path
+ *         name: page
+ *         required: true
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         description: Página desejada
+ *     responses:
+ *       '200':
+ *           description: 'Requisição bem sucedida.'
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   date:
+ *                     type: object
+ *                   status:
+ *                     type: boolean
+ *                   data:
+ *                     type: string
+ *                     description: 'objeto json de retorno'
+ *               example:
+ *                 date: {}
+ *                 status: true
+ *                 data: <ARRAY DE PERSONAGENS JSON>
+ *       '404':
+ *           description: 'Requisição falhou.'
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   date:
+ *                     type: object
+ *                   status:
+ *                     type: boolean
+ *                   data:
+ *                     type: string
+ *                     description: 'objeto json de retorno'
+ *               example:
+ *                 date: {}
+ *                 status: false
+ *                 data: "Página não encontrada."
+ *       '500':
+ *           description: 'Erro interno.'
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   date:
+ *                     type: object
+ *                   status:
+ *                     type: boolean
+ *                   data:
+ *                     type: string
+ *                     description: 'objeto json de retorno'
+ *               example:
+ *                 date: {}
+ *                 status: false
+ *                 data: "Um erro interno ocorreu."
+ *
+ */
 export class CharacterController {
   async getCharactersPage(req: Request, res: Response) {
     try {
@@ -15,6 +90,14 @@ export class CharacterController {
         take: limit,
         skip: offset
       });
+
+      if (characters.length === 0) {
+        return res.status(404).send({
+          date: new Date(),
+          status: false,
+          data: 'Página não encontrada.'
+        });
+      }
 
       return res
         .status(200)
