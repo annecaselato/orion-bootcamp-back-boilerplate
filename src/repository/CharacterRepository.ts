38ffafb89b4065aaef2f0864c3e8bdeb31c016c2
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2023-11-08 22:04:46 -0300

    feat(charactersRepository): inclui função de busca e atualização do database ao repositorio

--- src/repository/CharacterRepository.ts
+++ src/repository/CharacterRepository.ts
@@ -1,30 +1,71 @@
 import { MysqlDataSource } from '../config/database';
-import { Request, Response } from 'express';
+import identical from '../utils/charactersRepoHelpers';
 import { Character } from '../entity/Character';
 import CharacterModel from 'library/CharacterInterface';
 
 export default class CharacterRepository {
   private repository = MysqlDataSource.getRepository(Character);
 
-  constructor() {}
-
-  async createAndSave(
-    untranslatedCharacters: Array<CharacterModel>,
-    translatedCharacters?: Array<CharacterModel>,
-    req?: Request,
-    res?: Response
-  ): Promise<void> {
-    const characters: CharacterModel[] = translatedCharacters
-      ? translatedCharacters
-      : untranslatedCharacters;
-
+  async updateOrSave(characters: Array<CharacterModel>): Promise<void> {
     characters.forEach(async (character: CharacterModel) => {
-      const newCharacter = new Character();
-      newCharacter.description = character.description;
-      newCharacter.thumb = character.thumbnail;
-      newCharacter.pt_name = character.name;
+      const characterRegistered = await this.findOneByMarvelId(
+        character.idMarvel
+      );
 
-      await this.repository.manager.save(newCharacter);
+      if (!characterRegistered) {
+        this.createAndSave(character);
+      } else if (identical(character, characterRegistered)) {
+        return;
+      } else {
+        MysqlDataSource.createQueryBuilder()
+          .update(Character)
+          .set({
+            enName: character.enName,
+            ptName: character.ptName,
+            description: character.description,
+            thumb: character.thumb,
+            isTranslated: character.isTranslated,
+            lastUpdate: new Date()
+          })
+          .where('idMarvel = :idMarvel', { idMarvel: character.idMarvel })
+          .execute();
+      }
     });
+  }
+
+  private async createAndSave(
+    character?: CharacterModel,
+    characters?: Array<CharacterModel>
+  ): Promise<void> {
+    if (characters) {
+      characters.forEach((character: CharacterModel) => {
+        this.save(character);
+      });
+    }
+
+    if (character) {
+      this.save(character);
+    }
+  }
+
+  private async save(character: CharacterModel): Promise<void> {
+    const newCharacter = new Character();
+    newCharacter.idMarvel = character.idMarvel;
+    newCharacter.enName = character.enName;
+    newCharacter.ptName = character.ptName;
+    newCharacter.description = character.description;
+    newCharacter.thumb = character.thumb;
+    newCharacter.isTranslated = character.isTranslated;
+    newCharacter.lastUpdate = new Date();
+    await this.repository.manager.save(newCharacter);
+  }
+
+  private async findOneByMarvelId(idMarvel: number): Promise<Character> {
+    const characterRegistered = await this.repository.findOne({
+      where: {
+        idMarvel: idMarvel
+      }
+    });
+    return characterRegistered;
   }
 }
