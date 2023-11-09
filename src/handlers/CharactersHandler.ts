a71602d651355e64689c2cd9bf5fa52f5f36cadb
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2023-11-08 22:03:22 -0300

    refactor:(CharactersHandler): altera classe para chamada por job

--- src/handlers/CharactersHandler.ts
+++ src/handlers/CharactersHandler.ts
@@ -1,21 +1,13 @@
 import axios from 'axios';
-import MarvelCharactersProperties from '../library/charactersPropertiesInterface';
 import MarvelAPIParams from '../library/marvelAPIParamsInterface';
 import * as marvelGetHelpers from '../utils/marvelGetHelpers';
 
 export default class CharactersHandler {
-  async getCharacters(): Promise<MarvelCharactersProperties[]> {
+  async getCharacters(offset) {
     try {
-      const page: number = marvelGetHelpers.initialPage();
       const timestamp = marvelGetHelpers.getTimestamp();
       const hash = await marvelGetHelpers.hashGenarator(timestamp);
-      const validPage = Math.min(marvelGetHelpers.maximunValidPage(), page); // evita retornar array vazio
-      const offset = (validPage - 1) * marvelGetHelpers.cardsPerPage(); // se page === 1, offset = 0
-
-      const limit = Math.min(
-        marvelGetHelpers.maxMarvelAPILimit(),
-        validPage * marvelGetHelpers.cardsPerPage()
-      ); // limit pode ser até o valor máximo definido pela API (100)
+      const limit = marvelGetHelpers.maxMarvelAPILimit(); // valor máximo definido pela API (100)
 
       const response = await axios.get<MarvelAPIParams>(
         `${marvelGetHelpers.baseURL()}/characters`,
@@ -29,9 +21,9 @@ export default class CharactersHandler {
           }
         }
       );
+
       //res.json(response.data);
-      const charactersData: Array<MarvelCharactersProperties> =
-        await response.data.data.results;
+      const charactersData: Array<unknown> = await response.data.data.results;
       return charactersData;
     } catch (error) {
       console.error(error);
