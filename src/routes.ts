6854dcadd6dd796c4c68107d9a79a3110bd2fab3
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2023-11-08 21:58:07 -0300

    feat(CharactersController): retira controller
    retira controller uma vez que se trata de atualização do banco através de schedule node-cron

--- src/routes.ts
+++ src/routes.ts
@@ -3,19 +3,12 @@ import { validationField, Validator } from './validator/userValidator';
 import { UserController } from './controller/UserController';
 import { AuthController } from './controller/AuthController';
 import { authenticateToken } from './middleware/AuthMiddleware';
-import CharactersController from './controller/CharactersController';
 
 const router = Router();
 
 //garantir apenas acesso autenticado à dashboard
 router.all('/v1/dashboard', authenticateToken, (req, res) => {
   res.sendStatus(200);
-});
-
-//TODO: colocar middleware de autenticação
-router.get('/v1/getCharacters/:page', authenticateToken, (req, res, next) => {
-  const page: number = Number(req.params.page) || 1;
-    new CharactersController().viewCharacters(req, res);
 });
 
 router.post('/v1/login', new AuthController().login);
