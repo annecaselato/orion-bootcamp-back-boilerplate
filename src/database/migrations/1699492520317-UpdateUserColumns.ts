830b3b09113cfa66cf6d24e14df59bc357eab79d
joaogabriellyra <jgabriellyra@hotmail.com>
2023-11-08 22:31:30 -0300

    feat: alterada função em desuso

--- src/database/migrations/1699492520317-UpdateUserColumns.ts
+++ src/database/migrations/1699492520317-UpdateUserColumns.ts
@@ -3,20 +3,24 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class UpdateUserColumns1699492520317 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.manager
-      .createQueryBuilder()
-      .update(User)
-      .set({ firstName: 'Gustavo', lastName: 'Gonçalves da Silva' })
-      .where('email = email', { email: 'gustavogonçalves@gmail.com' })
-      .execute();
+    await queryRunner.manager.update(
+      User,
+      { email: 'gustavogonçalves@gmail.com' },
+      {
+        firstName: 'Gustavo',
+        lastName: 'Gonçalves da Silva'
+      }
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.manager
-      .createQueryBuilder()
-      .update(User)
-      .set({ firstName: '', lastName: '' })
-      .where('email = email', { email: 'gustavogonçalves@gmail.com' })
-      .execute();
+    await queryRunner.manager.update(
+      User,
+      { email: 'gustavogonçalves@gmail.com' },
+      {
+        firstName: '',
+        lastName: ''
+      }
+    );
   }
 }
