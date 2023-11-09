0c6ca44ef858d5094c212b14add42b1373a2a42e
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2023-11-08 22:01:49 -0300

    feat(FormatHandler): insere filtro e inclui validações adicionais

--- src/handlers/FormatHandler.ts
+++ src/handlers/FormatHandler.ts
@@ -1,6 +1,7 @@
 import { Translate } from '@google-cloud/translate/build/src/v2';
-import MarvelCharactersProperties from '../library/charactersPropertiesInterface';
-import { extractCharacters } from '../utils/formatHelpers';
+import CharacterModel from '../library/CharacterInterface';
+import { extractCharacters, imageNotAviable } from '../utils/formatHelpers';
+import 'dotenv/config';
 
 const apiKey = process.env.GOOGLE_TRANSLATE_API_KEY; //Key da API google Translate
 const projectId = process.env.GOOGLE_TRANSLATE_PROJECT_ID; // ID do projeto google Translate
@@ -8,29 +9,60 @@ const projectId = process.env.GOOGLE_TRANSLATE_PROJECT_ID; // ID do projeto goog
 export default class FormatHandler {
   translateClient = new Translate({ projectId, key: apiKey });
 
-  async extractCharactersAndTryTotranslate(charactersData) {
+  async extractAndTryTotranslate(charactersData): Promise<CharacterModel[]> {
     try {
-      const translatedCharacters = await Promise.all(
-        charactersData.map(async (character: MarvelCharactersProperties) => {
-          const [nameTranslation]: string =
+      const formattedCharacters: CharacterModel[] = await Promise.all(
+        charactersData.map(async (character) => {
+          const [nameTranslation]: string | undefined =
             await this.translateClient.translate(character.name, 'pt');
-          const [descriptionTranslation]: string =
+
+          const [descriptionTranslation]: string | undefined =
             await this.translateClient.translate(character.description, 'pt');
-          const characterThumb: string = `${character.thumbnail.path}.jpg`;
-          const translatedCharacters = {
-            name: nameTranslation,
+
+          const path: string = character.thumbnail.path;
+
+          const characterThumb: string | undefined = imageNotAviable(path)
+            ? ''
+            : `${character.thumbnail.path}.jpg`;
+
+          const translated: boolean = true;
+          const idMarvel: number = character.id;
+          const enName: string = character.name;
+
+          const formattedCharacters: CharacterModel = {
+            idMarvel: idMarvel,
+            enName: enName,
+            ptName: nameTranslation,
             description: descriptionTranslation,
-            thumbnail: characterThumb
+            thumb: characterThumb,
+            isTranslated: translated
           };
-          return translatedCharacters;
+          return formattedCharacters;
         })
       );
-      return { translated: true, translatedCharacters };
+
+      return formattedCharacters;
     } catch (error) {
       // Se houver erro na tradução, retorna o personagem original não traduzido
       console.error('Não foi possível traduzir os dados');
-      const untranslatedCharacters = extractCharacters(charactersData);
-      return { translated: false, untranslatedCharacters };
+      const extractedCharacters: CharacterModel[] =
+        extractCharacters(charactersData);
+
+      return extractedCharacters;
     }
+  }
+
+  // criterios de filtro a serem definidos na proxima sprint
+  private filterValidCharacters(
+    formattedCharacters: Array<CharacterModel>
+  ): Array<CharacterModel> {
+    const filteredArray = formattedCharacters.filter((character) => {
+      return (
+        (character.enName || character.ptName) &&
+        character.description &&
+        character.thumb
+      );
+    });
+    return filteredArray;
   }
 }
