6ba47e04de2079d1b6d4409fbf78c7e91a5e4737
Ricardo Miranda de Novais <ricardonovais1@gmail.com>
2023-11-10 13:45:13 -0300

    refactor: add class authValidation Middleware

--- src/middlewares/authValidation.ts
+++ src/middlewares/authValidation.ts
@@ -2,40 +2,42 @@ import { NextFunction, Request, Response } from 'express';
 import { JwtUtils } from '../library/jwtUtils';
 import { UserRepository } from '../repositories/userRepository';
 
-/**
- * Middleware for authentication and authorization.
- * @param req - The Express request object.
- * @param res - The Express response object.
- * @param next - The next middleware function.
- */
-export async function authValidation(req: Request, res: Response, next: NextFunction): Promise<void> {
-  const accessToken = req.headers.authorization;
-  const verifiedToken = await JwtUtils.verifyJWTToken(accessToken);
+export class AuthMiddleware {
+  /**
+   * Middleware for authentication and authorization.
+   * @param req - The Express request object.
+   * @param res - The Express response object.
+   * @param next - The next middleware function.
+   */
+  public static async authValidation(req: Request, res: Response, next: NextFunction): Promise<void> {
+    const accessToken = req.headers.authorization;
+    const verifiedToken = await JwtUtils.verifyJWTToken(accessToken);
 
-  if (verifiedToken.error) {
-    const errorMessage =
-      {
-        'jwt must be provided': 'Access token deve ser fornecido',
-        'invalid signature': 'Access token inválido',
-        'jwt expired': 'Access token expirado'
-      }[verifiedToken.error.message] || 'Não autorizado';
+    if (verifiedToken.error) {
+      const errorMessage =
+        {
+          'jwt must be provided': 'Access token deve ser fornecido',
+          'invalid signature': 'Access token inválido',
+          'jwt expired': 'Access token expirado'
+        }[verifiedToken.error.message] || 'Não autorizado';
 
-    res.status(401).json({ message: errorMessage });
-    return;
-  }
-
-  const userId = verifiedToken.id;
-
-  try {
-    const user = await UserRepository.findUserById(userId);
-
-    if (user.accessToken !== accessToken.split(' ')[1]) {
-      res.status(401).json({ message: 'Access token inválido' });
+      res.status(401).json({ message: errorMessage });
       return;
     }
 
-    next();
-  } catch (error) {
-    res.status(500).json({ message: 'Erro interno do servidor' });
+    const userId = verifiedToken.id;
+
+    try {
+      const user = await UserRepository.findUserById(userId);
+
+      if (user.accessToken !== accessToken.split(' ')[1]) {
+        res.status(401).json({ message: 'Access token inválido' });
+        return;
+      }
+
+      next();
+    } catch (error) {
+      res.status(500).json({ message: 'Erro interno do servidor' });
+    }
   }
 }
