90bcde3d5f773ab0ff3b6ff5c28fdbe6e5d16263
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2023-11-14 09:50:44 -0300

    perf: altera função de tradução para avaliação por propriedade traduzida e não mais por objeto     altera funções auxiliares para abranger todas as categorias

---
+++ src/utils/formatterHelpers.ts
@@ -0,0 +1,103 @@
+import { Translate } from '@google-cloud/translate/build/src/v2';
+import CategoryModel from '../library/CategoryInterface';
+import 'dotenv/config';
+
+const apiKey = process.env.GOOGLE_TRANSLATE_API_KEY; //Key da API google Translate
+const projectId = process.env.GOOGLE_TRANSLATE_PROJECT_ID; // ID do projeto google Translate
+const translateClient = new Translate({ projectId, key: apiKey });
+
+// avalia tradução de cada propriedade e não mais de todo o objeto
+export async function extractAndTryTotranslate(
+  object,
+  Category
+): Promise<CategoryModel> {
+  let translated: boolean = true;
+  let description: string = '';
+
+  try {
+    if (object.description) {
+      const [descriptionTranslation] = await translateClient.translate(
+        object.description,
+        'pt'
+      );
+      description = descriptionTranslation;
+    }
+  } catch (error) {
+    description = object.description;
+    translated = false;
+    console.error(
+      `Não foi possível traduzir description do objeto: ${Category.name} IdMarvel: `,
+      object.id,
+      '\n' + error
+    );
+  }
+
+  const formattedObject: CategoryModel = {
+    idMarvel: object.id,
+    description: description,
+    thumb: thumbFormatter(object),
+    isTranslated: translated
+  };
+
+  let identifier;
+
+  if (object.name) {
+    try {
+      const [nameTranslation] = await translateClient.translate(
+        object.name,
+        'pt'
+      );
+      formattedObject.enName = object.name;
+      formattedObject.ptName = nameTranslation;
+    } catch (error) {
+      formattedObject.enName = object.name;
+      formattedObject.ptName = 'not available';
+      translated = false;
+      console.error(
+        `Não foi possível traduzir o nome do objeto: ${Category.name} IdMarvel: `,
+        object.id,
+        '\n' + error
+      );
+    }
+  } else {
+    try {
+      const [titleTranslation] = await translateClient.translate(
+        object.title,
+        'pt'
+      );
+      formattedObject.enTitle = object.title;
+      formattedObject.ptTitle = titleTranslation;
+    } catch (error) {
+      formattedObject.enTitle = object.title;
+      formattedObject.ptTitle = 'not available';
+      translated = false;
+      console.error(
+        `Não foi possível traduzir o título do objeto: ${Category.name} IdMarvel: `,
+        object.id,
+        '\n' + error
+      );
+    }
+  }
+  formattedObject.isTranslated = translated;
+  return formattedObject;
+}
+
+export function thumbFormatter(object): string {
+  let thumb: string = '';
+
+  // stories não possui path
+  if ((object.resourceURI as string).includes('stories')) {
+    const thumbnail: string | undefined = object.thumbnail;
+    thumb =
+      thumbnail && !thumbnail.includes('not_available')
+        ? `${thumbnail}.jpg`
+        : '';
+  } else {
+    const thumbnail: string = object.thumbnail.path;
+    thumb =
+      thumbnail && !thumbnail.includes('not_available')
+        ? `${thumbnail}.jpg`
+        : '';
+  }
+  return thumb;
+}
