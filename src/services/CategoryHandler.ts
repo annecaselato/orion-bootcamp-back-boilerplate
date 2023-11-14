62cd8510f1a6cffcac55d736512e14972684a142
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2023-11-14 09:30:49 -0300

    feat(categoryHandler): permite uso da classe por todas as Categorias         adiciona parâmetro modifiedSince à requisição visando execução por job         adiciona loop para get na API ao método da classe

---
+++ src/services/CategoryHandler.ts
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import MarvelAPIParams from '../library/marvelAPIParamsInterface';
+import * as marvelGetHelpers from '../utils/marvelGetHelpers';
+import 'dotenv/config';
+
+export default class CategoryHandler {
+  async getElements(categoryToURL): Promise<unknown[]> {
+    try {
+      const timestamp = marvelGetHelpers.getTimestamp();
+      const hash = await marvelGetHelpers.hashGenarator(timestamp);
+      const limit = marvelGetHelpers.maxMarvelAPILimit(); // máximo definido pela API: 100
+      const offset = marvelGetHelpers.offsetter();
+
+      let dataArray: Array<unknown> = [];
+      const categoryData: Array<unknown> = [];
+
+      do {
+        const response = await axios.get<MarvelAPIParams>(
+          `${marvelGetHelpers.baseURL()}/${categoryToURL}`,
+          {
+            params: {
+              offset: offset.next().value,
+              limit: limit,
+              ts: timestamp,
+              apikey: process.env.MARVEL_API_KEY,
+              modifiedSince: marvelGetHelpers.sevenDaysAgo(),
+              hash: hash
+            }
+          }
+        );
+
+        dataArray = await response.data.data.results;
+        categoryData.push(...dataArray);
+      } while (dataArray.length);
+
+      console.log(
+        `${
+          categoryData.length
+        } dados modificados em ${categoryToURL} desde ${marvelGetHelpers.sevenDaysAgo()}`
+      );
+      return categoryData;
+    } catch (error) {
+      console.error(error);
+      return Promise.reject('Erro interno do servidor');
+    }
+  }
+}
