d369316132494f10f957d95de5da431d6cc327a3
joaogabriellyra <jgabriellyra@hotmail.com>
2023-11-15 17:12:34 -0300

    feat: agora o método retorna os últimos 7 soles

--- src/utils/NasaInsertWeatherApi.ts
+++ src/utils/NasaInsertWeatherApi.ts
@@ -6,16 +6,8 @@ export class NasaApi {
     const JSO = await axios.get(
       'https://mars.nasa.gov/rss/api/?feed=weather&category=msl&feedtype=json'
     );
-    const soles = JSO.data.soles.filter(
-      ({ sol }) =>
-        sol == 259 ||
-        sol == 260 ||
-        sol == 261 ||
-        sol == 262 ||
-        sol == 267 ||
-        sol == 268 ||
-        sol == 269
-    );
-    return soles;
+    const soles = JSO.data.soles;
+    const theLastSevenSoles = soles.slice(0, 7);
+    return theLastSevenSoles;
   }
 }
