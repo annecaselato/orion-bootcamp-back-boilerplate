aa49ebc89a2cbe1195837bb8b93c3aa9a9d9baf7
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2023-11-16 18:44:41 -0300

    perf: utiliza método específico upsert, reduzindo processamento para buscas e atualizações

--- src/repository/CategoryRepositoy.ts
+++ src/repository/CategoryRepositoy.ts
@@ -1,29 +1,9 @@
-import * as repoAuxiliary from '../utils/categoryRepoHelpers';
+import { MysqlDataSource } from '../config/database';
 import CategoryModel from 'library/CategoryInterface';
 
 export default class CategoryRepository {
   async updateOrSave(objectsArray: CategoryModel[], Category): Promise<void> {
-    const objectsToSave: Array<CategoryModel> = [];
-    const objectsToUpdate: Array<CategoryModel> = [];
-
-    for (const object of objectsArray) {
-      const objectRegistered = await repoAuxiliary.findOneByMarvelId(
-        object.idMarvel,
-        Category
-      );
-
-      if (!objectRegistered) {
-        objectsToSave.push(object);
-      } else if (!repoAuxiliary.identical(object, objectRegistered)) {
-        objectsToUpdate.push(object);
-      }
-    }
-
-    if (objectsToSave.length) {
-      await repoAuxiliary.createAndSave(objectsToSave, Category);
-    }
-    if (objectsToUpdate.length) {
-      await repoAuxiliary.update(objectsToUpdate, Category);
-    }
+    const repository = MysqlDataSource.getRepository(Category);
+    await repository.upsert(objectsArray, ['idMarvel']);
   }
 }
