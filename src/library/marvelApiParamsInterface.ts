e8d41fd9ff8ab2f119863d32a6af9df243c5163d
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2023-11-16 20:28:09 -0300

    chore: correções prettier

--- src/library/marvelApiParamsInterface.ts
+++ src/library/marvelApiParamsInterface.ts
@@ -1,9 +1,8 @@
 export default interface MarvelAPIParams {
-    data: unknown;
-    offset: number;
-    limit: number;
-    ts: number;
-    apikey: string;
-    hash: string;
-  }
-  
+  data: unknown;
+  offset: number;
+  limit: number;
+  ts: number;
+  apikey: string;
+  hash: string;
+}
