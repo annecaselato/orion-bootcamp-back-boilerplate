399467868483a2399fd569fbda0dfdf96c42aa0b
joaogabriellyra <jgabriellyra@hotmail.com>
2023-11-17 15:27:52 -0300

    feat: alterado nome do método em que chama os sóis de marte

--- src/utils/NasaApi.ts
+++ src/utils/NasaApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import ISol from '../interfaces/ISol';
 
 export class NasaApi {
-  public async getSolesInInsertWeatherApi(): Promise<ISol[]> {
+  public async getSolesInWeatherApi(): Promise<ISol[]> {
     const JSO = await axios.get(process.env.MARTE_API);
     const soles = JSO.data.soles;
     const theMostRecentSevenSoles = soles.slice(0, 7);
