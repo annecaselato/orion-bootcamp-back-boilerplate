1c5993286e8641d9ab9769c590dbcdfa4f43e8ba
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2023-11-17 11:29:26 -0300

    docs: adiciona documentação à interface de categorias

--- src/models/CategoryInterface.ts
+++ src/models/CategoryInterface.ts
@@ -1,11 +1,53 @@
-// interface das categorias Character, Serie, Comic, Event e Storie
+/**
+ * Interface de categorias Marvel: Character, Series, Comic, Event e Story
+ */
 export default interface CategoryModel {
+  /** Número de ID atribuído pela Marvel */
   idMarvel: number;
+  /**
+   * Nome original não traduzido.
+   *
+   * Atributo exclusivo da categoria Character.
+   * Not null para essa categoria.
+   */
   enName?: string;
+  /**
+   * Nome traduzido.
+   *
+   * Atributo exclusivo da categoria Character.
+   * Será \'' se não disponível.
+   */
   ptName?: string;
+  /**
+   * Título original não traduzido.
+   *
+   * Atributo específico das categorias: Series, Comic, Event e Story.
+   * Not null para essas categorias.
+   */
   enTitle?: string;
+  /**
+   * Título traduzido.
+   *
+   * Atributo específico das categorias: Series, Comic, Event e Story.
+   * Será \'' se não disponível.
+   */
   ptTitle?: string;
+  /**
+   * Descrição  da categoria, traduzida se possível.
+   *
+   * Será \'' se não disponível.
+   */
   description: string;
+  /**
+   * Thumbnail  da categoria.
+   *
+   * Será \'' se não disponível.
+   */
   thumb: string;
+  /**
+   * Indicação de tradução de atributos.
+   *
+   * Será true apenas se todos atributos não nulos forem traduzidos.
+   */
   isTranslated: boolean;
 }
