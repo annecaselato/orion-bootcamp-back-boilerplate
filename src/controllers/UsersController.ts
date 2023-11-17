29fc8c5f2e660ee6501d8541bf1fd1db2d8d9bfd
joaogabriellyra <jgabriellyra@hotmail.com>
2023-11-17 16:51:41 -0300

    feat: criado método na camada de controller em que retorna a requisição dos sóis de acordo com a assinatura do usuário ou retorna erro

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
-import { UserService } from '../services/UserService';
-import { httpCodes } from '../utils/httpCodes';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { UserService } from '../services/UserService';
+import { httpCodes } from '../utils/httpCodes';
+import NasaApi from '../utils/NasaApi';
+import WeatherApiFilter from '../utils/WeatherApiFilter';
 
 type JwtPayload = {
   id: number;
@@ -320,6 +322,69 @@ export class UsersController {
       return res
         .status(httpCodes.UNAUTHORIZED)
         .json({ mensagem: 'User not found.' });
+    }
+  }
+
+  /**
+   * @swagger
+   * /users/soles:
+   *   post:
+   *     summary: Rota para receber os sóis e suas informações.
+   *     tags: [Users]
+   *     consumes:
+   *       - application/json
+   *     produces:
+   *       - application/json
+   *     requestBody:
+   *         required: true
+   *         content:
+   *           application/json:
+   *             schema:
+   *               example:
+   *                 signature: premium
+   *               type: object
+   *               properties:
+   *                 signature:
+   *                   type: string
+   *     responses:
+   *       '200':
+   *           description: 'Requisição dos sóis baseado na assinatura'
+   *           content:
+   *             application/json:
+   *               schema:
+   *                 type: object
+   *                 properties:
+   *                   sol:
+   *                     type: string
+   *                   max_temp:
+   *                     type: string
+   *                   min_temp:
+   *                     type: string
+   *                   terrestrial_date:
+   *                     type: Date
+   *                   pressure:
+   *                     type: string
+   *                   local_uv_irradiance_index:
+   *                     type: string
+   *                   sunrise:
+   *                     type: string
+   *                   sunset:
+   *                     type: string
+   *       '400':
+   *           description: 'Solicitação inválida.'
+   */
+  async getSolesInWeatherApi(req: Request, res: Response) {
+    const { signature } = req.body;
+    const nasaApi = new NasaApi();
+    try {
+      const solesInput = await nasaApi.getSolesInWeatherApi();
+      const solesOutput = new WeatherApiFilter().filterSolesKeysBySignature(
+        signature,
+        solesInput
+      );
+      return res.status(httpCodes.OK).json(solesOutput);
+    } catch (error) {
+      return res.status(httpCodes.BAD_REQUEST).json(error);
     }
   }
 }
