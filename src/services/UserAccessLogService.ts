6e8869be64c3cd602e629628bee58f4084c362ab
pablo <68402065+itzinn@users.noreply.github.com>
2023-11-21 08:54:37 -0300

    docs: adiciona documentação typedoc ao serviço UserAccessLogService

--- src/services/UserAccessLogService.ts
+++ src/services/UserAccessLogService.ts
@@ -2,7 +2,14 @@ import { MysqlDataSource } from '../config/database';
 import UserAccessLog from '../entity/UserAccessLog';
 import User from '../entity/User';
 
+/**
+ * Classe que possui métodos úteis para o registro de logins de usuários no banco.
+ */
 export default class UserAccessLogService {
+
+  /**
+   * @param {User} user - Usuário que realizou o login
+   */
   static async logUserAccess(user: User) {
     const userAccessLogRepository =
       MysqlDataSource.getRepository(UserAccessLog);
