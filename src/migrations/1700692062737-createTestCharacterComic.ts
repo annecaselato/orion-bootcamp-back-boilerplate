ffad655c993d070e6645bc99fd174b2ffd4d8576
pablo <68402065+itzinn@users.noreply.github.com>
2023-11-22 20:18:55 -0300

    chore: cria migration que insere valores de teste na entidade CharacterComics

---
+++ src/migrations/1700692062737-createTestCharacterComic.ts
@@ -0,0 +1,60 @@
+import { MysqlDataSource } from '../config/database';
+import Character from '../entity/Character';
+import { CharacterComics } from '../entity/CharacterComics';
+import Comic from '../entity/Comic';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTestCharacterComic1700692062737
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const characterRepository = MysqlDataSource.getRepository(Character);
+    const comicRepository = MysqlDataSource.getRepository(Comic);
+
+    const character1: Character = await characterRepository.findOne({
+      where: {
+        id: 1
+      }
+    });
+
+    const comic1: Comic = await comicRepository.findOne({
+      where: {
+        id: 1
+      }
+    });
+
+    const comic2: Comic = await comicRepository.findOne({
+      where: {
+        id: 1
+      }
+    });
+
+    const comic3: Comic = await comicRepository.findOne({
+      where: {
+        id: 1
+      }
+    });
+
+    await queryRunner.manager
+      .createQueryBuilder()
+      .insert()
+      .into(CharacterComics)
+      .values([
+        {
+          character: character1,
+          comic: comic1
+        },
+        {
+          character: character1,
+          comic: comic2
+        },
+        {
+          character: character1,
+          comic: comic3
+        }
+      ])
+      .execute();
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
