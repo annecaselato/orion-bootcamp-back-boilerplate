e1305e5a3b39df19f371670289952659b130cc41
joaogabriellyra <jgabriellyra@hotmail.com>
2023-11-22 22:08:23 -0300

    feat: alterada a forma como retornava a data. antes era a data americana e agora retorna o dia em data e o mês em português

--- src/utils/WeatherApiTreat.ts
+++ src/utils/WeatherApiTreat.ts
@@ -2,11 +2,31 @@ import ISolModified from '../interfaces/ISolModified';
 import ISol from '../interfaces/ISol';
 
 export default class WeatherApiTreat {
+  private modifyTerrestrialDate(date: Date): string {
+    const months = [
+      'Janeiro',
+      'Fevereiro',
+      'Março',
+      'Abril',
+      'Maio',
+      'Junho',
+      'Julho',
+      'Agosto',
+      'Setembro',
+      'Outubro',
+      'Novembro',
+      'Dezembro'
+    ];
+    const numberDay = date[8] !== '0' ? `${date[8]}${date[9]}` : date[9];
+    const monthIndex = date[5] !== '0' ? `${date[5]}${date[6]}` : date[6];
+    return `${numberDay} ${months[Number(monthIndex) - 1]}`;
+  }
+
   public modifySolesKeys(solesInput: ISol[]): ISolModified[] {
     const solesOutput = solesInput.map(
       ({ sol, terrestrial_date, max_temp, min_temp }) => ({
         sol,
-        terrestrial_date,
+        terrestrial_date: this.modifyTerrestrialDate(terrestrial_date),
         max_temp,
         min_temp
       })
