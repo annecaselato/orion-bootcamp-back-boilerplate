7a6b525a1b9b0fdb3caee5709a2f5131028b752a
Lorena <lorenaborgessilva@gmail.com>
2023-11-23 22:56:33 -0300

    atualização email usuario formato front

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,6 +7,6 @@ export const MysqlDataSource = new DataSource({
   url: process.env.DB_CONNECTION_STRING,
   entities: ['src/database/entity/*.ts', 'src/database/entity/*.js'],
   migrations: ['src/database/migrations/*.ts', 'src/database/migrations/*.js'],
-  logging: false,
+  logging: true,
   synchronize: true
 });
