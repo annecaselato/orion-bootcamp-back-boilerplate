621e109cd289f0d63cbbc4b18d972373ed53cfdb
pablo <68402065+itzinn@users.noreply.github.com>
2023-12-01 22:42:10 -0300

    feat: cria endpoint que obtem posters aleatorios de artistas

--- src/entity/Artist.ts
+++ src/entity/Artist.ts
@@ -1,47 +1,45 @@
 import {
-    Entity,
-    Column,
-    PrimaryGeneratedColumn,
-    CreateDateColumn,
-  } from 'typeorm';
-  
-  /**
-   * Entidade com informações relacionadas a artistas parceiros
-   */
-  @Entity('artists')
-  export default class Artist {
-    @PrimaryGeneratedColumn()
-    id: number;
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn
+} from 'typeorm';
 
-    @CreateDateColumn()
-    registeredAt: Date;
-  
-    @Column({ type: 'varchar', length: 250, unique: true })
-    email: string;
+/**
+ * Entidade com informações relacionadas a artistas parceiros
+ */
+@Entity('artists')
+export default class Artist {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ type: 'varchar', length: 50 })
-    fullName: string;
-  
-    @Column({ type: 'varchar', length: 1024 })
-    interestOrigin: string;
+  @CreateDateColumn()
+  registeredAt: Date;
 
-    @Column({ type: 'varchar', length: 256 })
-    experience: string;
+  @Column({ type: 'varchar', length: 250, unique: true })
+  email: string;
 
-    @Column({ type: 'varchar', length: 256 })
-    artAsIncome: string;
+  @Column({ type: 'varchar', length: 50 })
+  fullName: string;
 
-    @Column({ type: 'varchar', length: 1024 })
-    portfolio: string;
+  @Column({ type: 'varchar', length: 1024 })
+  interestOrigin: string;
 
-    @Column({ type: 'int' })
-    artsPerMonth: number;
+  @Column({ type: 'varchar', length: 256 })
+  experience: string;
 
-    @Column({ type: 'varchar', length: 256 })
-    artSampleURL: string;
+  @Column({ type: 'varchar', length: 256 })
+  artAsIncome: string;
 
-    @Column({ type: 'varchar', length: 30 })
-    number: string;
+  @Column({ type: 'varchar', length: 1024 })
+  portfolio: string;
 
-  }
-  
+  @Column({ type: 'int' })
+  artsPerMonth: number;
+
+  @Column({ type: 'varchar', length: 256 })
+  artSampleURL: string;
+
+  @Column({ type: 'varchar', length: 30 })
+  number: string;
+}
