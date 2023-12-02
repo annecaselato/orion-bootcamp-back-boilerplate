91abe80fea536c8d6fef0e7735a493d69cd928d2
pablo <68402065+itzinn@users.noreply.github.com>
2023-12-02 17:42:47 -0300

    feat: refaz entidade artista

--- src/entity/Artist.ts
+++ src/entity/Artist.ts
@@ -1,45 +1,24 @@
 import {
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  CreateDateColumn
-} from 'typeorm';
-
-/**
- * Entidade com informações relacionadas a artistas parceiros
- */
-@Entity('artists')
-export default class Artist {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @CreateDateColumn()
-  registeredAt: Date;
-
-  @Column({ type: 'varchar', length: 250, unique: true })
-  email: string;
-
-  @Column({ type: 'varchar', length: 50 })
-  fullName: string;
-
-  @Column({ type: 'varchar', length: 1024 })
-  interestOrigin: string;
-
-  @Column({ type: 'varchar', length: 256 })
-  experience: string;
-
-  @Column({ type: 'varchar', length: 256 })
-  artAsIncome: string;
-
-  @Column({ type: 'varchar', length: 1024 })
-  portfolio: string;
-
-  @Column({ type: 'int' })
-  artsPerMonth: number;
-
-  @Column({ type: 'varchar', length: 256 })
-  artSampleURL: string;
-
-  @Column({ type: 'varchar', length: 30 })
-  number: string;
-}
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+  } from 'typeorm';
+  
+  /**
+   * Entidade com informações relacionadas a artistas parceiros
+   */
+  @Entity('artists')
+  export default class Artist {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column({ type: 'varchar', length: 250, unique: true })
+    email: string;
+  
+    @Column({ type: 'varchar', length: 50 })
+    fullName: string;
+  
+    @Column({ type: 'varchar', length: 256 })
+    artSampleURL: string;
+  }
+  
